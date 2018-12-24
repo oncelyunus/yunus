42d09a2bb962d48d60f8d7e7006e6963694c5a5e
Yunus <yunusoncel@gmail.com>
2018-12-24 09:47:27 +0300

    first commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ http.createServer(function(req, res) {
         console.log('URL', url);
     console.log('req.url', req.url);
     console.log('err', err);
+    alert(req.url);
         if (err) {
             var message404 = "There is no such page! <a href='/'>Back to home page</a>"
             res.writeHead(404, {'Content-Type': 'text/html', 'Content-Length': message404.length})
