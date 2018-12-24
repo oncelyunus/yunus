e02b660b040b5ba5e280e3c26af83a07ff2dd3d5
Yunus <yunusoncel@gmail.com>
2018-12-24 10:51:01 +0300

    .

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-var http = require('http'),
-    fs = require('fs')
-var port = process.env.PORT || 8080;
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
 
 
+
+http.createServer(function(req, res) {
+    var url =  (req.url == '/' ? './index.html' : '.'+req.url);
+    fs.readFile(url, function(err, html) {
+        if (err) {
+            var message404 = "There is no such page! <a href='/'>Back to home page</a>"
+            res.writeHead(404, {'Content-Type': 'text/html', 'Content-Length': message404.length})
+            res.write(message404)
+        } else {
+            //res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': html.length})
+            if(req.url.indexOf('.html') != -1) {
+                res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': html.length})
+            } else if(req.url.indexOf('.js') != -1) {
+                res.writeHead(200, {'Content-Type': 'text/javascript', 'Content-Length': html.length})
+            } else if(req.url.indexOf('.css') != -1) {
+                res.writeHead(200, {'Content-Type': 'text/css', 'Content-Length': html.length})
+            } else if(req.url.indexOf('.svg') != -1) {
+                res.writeHead(200, {'Content-Type': 'image/svg+xml', 'Content-Length': html.length})
+            }
+            res.write(html)
+        }
+        res.end()
+    })
+}).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
+
+/*
 http.createServer(function(req, res) {
     var url = './' + (req.url == '/' ? 'index.html' : req.url)
     
@@ -10,7 +41,6 @@ http.createServer(function(req, res) {
         console.log('URL', url);
     console.log('req.url', req.url);
     console.log('err', err);
-    alert(req.url);
         if (err) {
             var message404 = "There is no such page! <a href='/'>Back to home page</a>"
             res.writeHead(404, {'Content-Type': 'text/html', 'Content-Length': message404.length})
@@ -21,4 +51,4 @@ http.createServer(function(req, res) {
         }
         res.end()
     })
-}).listen(port);
+}).listen(port); */
